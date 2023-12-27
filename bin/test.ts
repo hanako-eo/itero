43f528f3646e1557982cbd824d8b36cff576508a
Hana <enuxura@gmail.com>
2023-12-27 18:51:40 +0100

    Remove unused dep

--- bin/test.ts
+++ bin/test.ts
@@ -1,11 +1,10 @@
 import { configure, processCLIArgs, run } from "@japa/runner"
 import { expect } from "@japa/expect"
-import { expectTypeOf } from "@japa/expect-type"
 
 processCLIArgs(process.argv.splice(2))
 configure({
     files: ["tests/**/*.spec.ts"],
-    plugins: [expect(), expectTypeOf()]
+    plugins: [expect()]
 })
 
 run()
